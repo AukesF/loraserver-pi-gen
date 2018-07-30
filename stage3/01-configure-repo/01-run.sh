fd263e1e3e1656d83d82d58509d9c3cf1256f24c
Orne Brocaar <info@brocaar.com>
2018-07-30 14:29:15 +0200

    Update apt repo to v2.

--- stage3/01-configure-repo/01-run.sh
+++ stage3/01-configure-repo/01-run.sh
@@ -4,7 +4,7 @@ on_chroot << EOF
 # loraserver
 rm -f /etc/apt/sources.list.d/loraserver.list
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1CE2AFD36DBCCA00
-echo "deb https://artifacts.loraserver.io/packages/1.x/deb stable main" | sudo tee /etc/apt/sources.list.d/loraserver.list
+echo "deb https://artifacts.loraserver.io/packages/2.x/deb stable main" | sudo tee /etc/apt/sources.list.d/loraserver.list
 
 apt-get update
 EOF
